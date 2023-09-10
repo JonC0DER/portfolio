e063463ff8d1a06b4a9f4334a4ba37c4df512440
jonC0D3R <joncoder.kamite@gmail.com>
2023-09-10 13:53:27 +0200

    created Menu component

---
+++ src/App/components/Menu.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <>
+      {/* MENU  */}
+      <section className="menu">
+        <ul>
+          <li className="roll">
+            <i></i>
+            <span className="menu-word"></span>
+          </li>
+          <li className="roll">
+            <i></i>
+            <span className="menu-word"></span>
+          </li>
+          <li className="roll">
+            <i></i>
+            <span className="menu-word"></span>
+          </li>
+          <li className="roll">
+            <i></i>
+            <span className="menu-word"></span>
+          </li>
+          <li className="roll">
+            <i></i>
+            <span className="menu-word"></span>
+          </li>
+          <li className="roll">
+            <i></i>
+            <span className="menu-word"></span>
+          </li>
+        </ul>
+      </section>
+    </>
+  )
+}
