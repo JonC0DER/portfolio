ca43619380bc29eed7e62d726696a0926015265c
jonC0D3R <joncoder.kamite@gmail.com>
2023-09-14 20:17:30 +0200

    created the menu component

--- src/App/components/Menu.tsx
+++ src/App/components/Menu.tsx
@@ -5,30 +5,68 @@ export default function Menu() {
     <>
       {/* MENU  */}
       <section className="menu">
+        <div className="menu-options">
+          <div className="large-small">
+            <span className="burger">
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </span>
+          </div>
+          <div className="themes">
+            <span className="circle">
+              <span className="material-symbols-outlined">dark_mode</span>
+            </span>
+          </div>
+        </div>
         <ul>
           <li className="roll">
-            <i></i>
-            <span className="menu-word"></span>
+            <span className="circle">
+              <span className="material-symbols-outlined">home</span>   
+            </span>
+            <span className="menu-word">Home</span>
           </li>
           <li className="roll">
-            <i></i>
-            <span className="menu-word"></span>
+            <span className="circle">
+              <span className="material-symbols-outlined">developer_mode_tv</span>
+            </span>
+            <span className="menu-word">Services</span>
           </li>
           <li className="roll">
-            <i></i>
-            <span className="menu-word"></span>
+            <span className="circle">
+              <span className="material-symbols-outlined">payments</span>
+            </span>
+            <span className="menu-word">Prices</span>
           </li>
           <li className="roll">
-            <i></i>
-            <span className="menu-word"></span>
+            <span className="circle">
+              <span className="material-symbols-outlined">recommend</span>
+            </span>
+            <span className="menu-word">Partners</span>
           </li>
           <li className="roll">
-            <i></i>
-            <span className="menu-word"></span>
+            <span className="circle">
+              <span className="material-symbols-outlined">school</span>
+            </span>
+            <span className="menu-word">Formations</span>
           </li>
           <li className="roll">
-            <i></i>
-            <span className="menu-word"></span>
+            <span className="circle">
+              <span className="material-symbols-outlined">work_history</span>
+            </span>
+            <span className="menu-word">Experiences</span>
+          </li> 
+          <li className="roll">
+            <span className="circle">
+              <span className="material-symbols-outlined">construction</span>
+            </span>
+            <span className="menu-word">Portfolio</span>
+          </li> 
+          <li className="roll">
+            <span className="circle">
+              <span className="material-symbols-outlined">contact_mail</span>
+            </span>
+            <span className="menu-word">Contacts</span>
           </li>
         </ul>
       </section>
