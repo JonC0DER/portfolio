0e2d7191fa47a272ec6a03e84adaac1568ec63fa
jonC0D3R <joncoder.kamite@gmail.com>
2023-09-14 20:19:31 +0200

    created the Hero component for the main part of the app

--- src/App/components/mainSiteComponents/Hero.tsx
+++ src/App/components/mainSiteComponents/Hero.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import HeroImg from '../../../assets/images/h.png';
 
 function Hero() {
   return (
     <>
       {/* hero  */}
-      <div className="hero"></div>
+      <div className="hero">
+        <div className="hero-text">
+          <h1 className="i-am">
+            Jonathan Jean-François <br />
+            <span className="fron-end">Front-End</span> Développeur
+          </h1>
+          <p className="presentation">
+            Je suis organisé, fiable et sans bugs !
+            <br />
+            flexibilité, créativité, esprit d'équipe.
+          </p>
+          <button className="hire-me">
+            <span className="btn-hire-me">ENGAGEZ-MOI</span> 
+            <span className="material-symbols-outlined">arrow_forward</span>
+          </button>
+        </div>
+        <figure>
+          <img src={HeroImg} alt="john!" />
+        </figure>
+      </div>
     </>
   )
 }
