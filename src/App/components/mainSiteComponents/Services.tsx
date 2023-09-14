3cd83efd34ae758c2ae18c335f8d18dbfcacf3b9
jonC0D3R <joncoder.kamite@gmail.com>
2023-09-14 20:19:56 +0200

    created the Services component for the main part of the app

--- src/App/components/mainSiteComponents/Services.tsx
+++ src/App/components/mainSiteComponents/Services.tsx
@@ -5,38 +5,39 @@ export default function Services() {
     <>
       {/* services  */}
       <section className="services">
-        <h2></h2>
-        <p></p>
+        <h2>Mes Services</h2>
+        <p>Lorem ipsum sed cupiditate eum, veniam quod quasi totam eos corporis culpa. Nostrum tempore accusantium ducimus cupiditate!</p>
         <div className="services-container">
           <div className="service">
-            <i></i>
-            <h3 className="title"></h3>
-            <p className="description"></p>
+            <div className="sprite web-dev"></div>
+            <h3 className="title">Web Développment</h3>
+            <p className="description">Application, Blog, E-commerce</p>
           </div>
           <div className="service">
-            <i></i>
-            <h3 className="title"></h3>
-            <p className="description"></p>
+            <div className="sprite web-design"></div>
+            <h3 className="title">Maquettage</h3>
+            <p className="description">Figma, Gimp, PhotoShop</p>
           </div>
           <div className="service">
-            <i></i>
-            <h3 className="title"></h3>
-            <p className="description"></p>
+            <div className="sprite back-end"></div>
+            <h3 className="title">Développement Back-End</h3>
+            <p className="description">Python (Flask), Adonis.js</p>
           </div>
           <div className="service">
-            <i></i>
-            <h3 className="title"></h3>
-            <p className="description"></p>
+            <div className="sprite dev-mobile"></div>
+            <h3 className="title">Développement Mobile</h3>
+            <p className="description">Avec React-Native</p>
           </div>
           <div className="service">
-            <i></i>
-            <h3 className="title"></h3>
-            <p className="description"></p>
+            <div className="sprite game-dev"></div>
+            <h3 className="title">Gaming Application</h3>
+            <p className="description">JavaScript / TypeScript PhaserJS</p>
           </div>
           <div className="service">
-            <i></i>
-            <h3 className="title"></h3>
-            <p className="description"></p>
+            <h3 className="title">Autres</h3>
+            <p className="description">
+              <br />
+            </p>
           </div>
         </div>
       </section>
