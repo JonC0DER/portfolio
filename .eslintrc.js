a94198ca4aab21cf440bd65a7f564a69169bf901
jonC0D3R <joncoder.kamite@gmail.com>
2023-10-14 12:21:29 +0200

    adding eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {},
+};
