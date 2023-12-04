9633c5ccb3e26869ec19d733a2b48f27ca7171b1
joncoder <joncoder.kamite@gmail.com>
2023-12-04 13:35:46 +0100

    adding & preparing JSON for MainSite & PresentationBoard composants

--- src/App/components/App.tsx
+++ src/App/components/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import PresentationBoard from './PresentationBoard';
 import MainSite from './MainSite';
 import Menu from './Menu';
+import jonData from '../../assets/JSON/data.json';
 
 export default function App() {
+  const presentationBoard = jonData.presentation_board;
+  const mainData = [
+    ...jonData.experiences,
+    ...jonData.formations,
+    ...jonData.miniatures_portfolio,
+  ];
+
   return (
     <div className="main-container">
-      <PresentationBoard />
-      <MainSite />
+      <PresentationBoard presentationData={presentationBoard} />
+      <MainSite data={...mainData} />
       <Menu />
     </div>
   );
