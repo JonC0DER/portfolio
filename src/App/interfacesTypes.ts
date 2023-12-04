93c3ece11363bf7c73c4ed915514e5f512d24697
joncoder <joncoder.kamite@gmail.com>
2023-12-04 13:33:51 +0100

    adding type PresentationType, FormationType, ExperienceType, MiniatureProtfolioType, DataJsonType

--- src/App/interfacesTypes.ts
+++ src/App/interfacesTypes.ts
@@ -1,11 +1,57 @@
-
 export type DevOptions = {
-  [key: string]: Boolean;
-}
+  [key: string]: boolean;
+};
 
 export type DevPrices = {
-  title: string, 
-  price: string, 
-  desc: string, 
-  options: DevOptions,
-}
+  title: string;
+  price: string;
+  desc: string;
+  options: DevOptions;
+};
+
+export type PresentationType = {
+  presentation: {
+    profil: string;
+    human_quality: string[];
+    contact: {
+      telephone: string;
+      email: string;
+      lastname: string;
+      firstname: string;
+      GitHub: string;
+      portfolio: string;
+    };
+  };
+  skills: {
+    systems: string[];
+    languages: {
+      [key: string]: string[];
+    };
+    linguistiques: string[];
+  };
+};
+
+export type FormationType = {
+  date: string;
+  school: string;
+  diploma?: string;
+};
+
+export type ExperienceType = {
+  date: string;
+  entreprise: string;
+  project: string;
+  description: string;
+  technologies: string[];
+};
+
+export type MiniaturePortfolioType = {
+  image: string;
+  link: string;
+};
+
+export type DataJsonType = [
+  formations: FormationType[],
+  experiences: ExperienceType[],
+  miniatures_portfolio: MiniaturePortfolioType[],
+];
