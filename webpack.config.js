6f06afb2f902c018baa2661974cb66b7626b2cc3
jonC0D3R <joncoder.kamite@gmail.com>
2024-06-13 15:30:26 +0200

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,49 @@
 // webpack.config.js
-// import path from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-// export default {
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'bundle.js',
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//       {
-//         test: /\.html$/,
-//         use: ['html-loader'],
-//       },
-//       // Ajoutez d'autres règles de loader au besoin
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './public/index.html',
-//       filename: 'index.html',
-//     }),
-//   ],
-//   // resolve: {
-//   //   fallback: {
-//   //     "os": require.resolve("os-browserify/browser")
-//   //   }
-//   // }
-// };
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      // Ajoutez d'autres règles de loader au besoin
+    ],
+  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: './public/index.html',
+  //     filename: 'index.html',
+  //   }),
+  // ],
+  // resolve: {
+  //   fallback: {
+  //     "os": require.resolve("os-browserify/browser")
+  //   }
+  // }
+};
